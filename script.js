40ab755a4bd2bfad298b81e3991acd6695dd266a
Pranit Karkera <pranitkarkera99@gmail.com>
2025-03-27 14:47:25 +0530

    update logic for drop-down

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("bundle-form");
+  // const form = document.getElementById("bundle-form");
   const pairSelections = document.querySelectorAll(".pair-selection");
+  const bundleOptions = document.querySelectorAll(".bundle-option");
 
   pairSelections.forEach((selection) => {
     selection.style.display = "none";
   });
 
-  form.addEventListener("change", function (event) {
-    pairSelections.forEach((selection) => {
-      selection.style.display = "none";
-    });
+  bundleOptions.forEach((option) => {
+    option.addEventListener("click", function () {
+      const radioButton = option.querySelector('input[type="radio"]');
+      if (radioButton) {
+        radioButton.checked = true;
 
-    function showPairSelection(optionId) {
-      if (event.target.id === optionId) {
-        const selectedOption = document
-          .getElementById(optionId)
-          .closest(".bundle-option");
-        const pairSelection = selectedOption.querySelector(".pair-selection");
+        pairSelections.forEach((selection) => {
+          selection.style.display = "none";
+        });
+
+        const pairSelection = option.querySelector(".pair-selection");
         if (pairSelection) {
-          pairSelection.style.display = "block";
+          pairSelection.style.display = "block"; 
         }
       }
-    }
-
-    showPairSelection("one-pair");
-    showPairSelection("two-pair");
-    showPairSelection("three-pair");
+    });
   });
 });
